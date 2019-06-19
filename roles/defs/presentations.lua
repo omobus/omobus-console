f912b3064e447dc54198a8d7d1fed694b832189e
Igor Artemov <i_artemov@omobus.net>
2019-06-19 12:57:23 +0300

    roles: fixed bugs.

--- roles/defs/presentations.lua
+++ roles/defs/presentations.lua
@@ -6,6 +6,6 @@ local M = {} -- public interface
 M.columns = {channel=true, head=true, brand=true}
 M.rows = 500
 M.csv = '{fix_dt},{head_name},{user_id},{u_name},{a_code},{a_name},{address},{rc},{chan},{region},{city},{tm_id},{tm},{brand},{participants},{doc_note},{doc_id}'
-M.zip = {photo='{doc_id}', max=1000}
+M.zip = {photo='{doc_id}-{blob_id}', max=1000}
 
 return M
