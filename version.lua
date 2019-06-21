ccf7653b51cf43b612cf8f8732731a35402a201c
Igor Artemov <i_artemov@omobus.net>
2019-06-21 15:16:41 +0300

    3.4.49 release

--- version.lua
+++ version.lua
@@ -4,7 +4,7 @@
 local M = {} -- public interface
 
 M.package_code		= "console"
-M.package_version	= "3.4.48"
+M.package_version	= "3.4.49"
 M.package_name		= "omobus-" .. M.package_code
 M.static_prefix		= "/" .. M.package_code .. "/" .. M.package_version
 
