bbc7870ca92d38546359f7e2b26989ced083bbb2
Igor Artemov <i_artemov@omobus.net>
2019-11-11 14:24:48 +0300

    stocks: initial commit.

---
+++ roles/defs/stocks.lua
@@ -0,0 +1,10 @@
+-- -*- Lua -*-
+-- Copyright (c) 2006 - 2019 omobus-console authors, see the included COPYRIGHT file.
+
+local M = {} -- public interface
+
+M.columns = {channel=true, head=true, brand=true, category=true}
+M.rows = 500
+M.csv = '{fix_dt},{head_name},{user_id},{u_name},{a_code},{a_name},{address},{rc},{chan},{region},{city},{p_code},{brand},{categ},{prod},{stock}'
+
+return M
