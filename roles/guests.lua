f6cb63c4688d555544136a4ee10485e943df3a4a
Igor Artemov <i_artemov@omobus.net>
2019-12-22 12:04:38 +0300

    roles: fixed [guests] role.

--- roles/guests.lua
+++ roles/guests.lua
@@ -3,48 +3,10 @@
 
 local M = {} -- public interface
 
-M.home = "tech"
-M.selectable = {
-    reports = {
-	daily = {"tech", "route_compliance"},
-	monthly = {"joint_routes", "confirmations", "orders", "reclamations", "comments", "photos", "testings", "trainings", "presentations", "audits", "shelfs", "quests", "advt", "prices"}
-    },
-    analitics = {"targets_compliance"},
-    managment = {"routes", "targets", "additions", "deletions", "wishes", "discards", "cancellations", "info_materials", "training_materials", "pos_materials", "planograms"}
-}
+M.home = "null"
 
-M.permissions = {
-    null 	= { },
-
-    additions 		= require 'roles.defs.additions',
-    advt 		= require 'roles.defs.advt',
-    audits 		= require 'roles.defs.audits',
-    cancellations 	= require 'roles.defs.cancellations',
-    comments 		= require 'roles.defs.comments',
-    confirmations 	= require 'roles.defs.confirmations',
-    deletions 		= require 'roles.defs.deletions',
-    discards 		= require 'roles.defs.discards',
-    info_materials 	= require 'roles.defs._ro',
-    joint_routes 	= require 'roles.defs.joint_routes',
-    orders 		= require 'roles.defs.orders',
-    photos 		= require 'roles.defs.photos',
-    planograms 		= require 'roles.defs._ro',
-    pos_materials 	= require 'roles.defs._ro',
-    presentations 	= require 'roles.defs.presentations',
-    prices 		= require 'roles.defs.prices',
-    quests 		= require 'roles.defs.quests',
-    reclamations 	= require 'roles.defs.reclamations',
-    route_compliance 	= require 'roles.defs.route_compliance',
-    routes 		= require 'roles.defs.routes',
-    shelfs 		= require 'roles.defs.shelfs',
-    targets 		= require 'roles.defs.targets',
-    targets_compliance 	= require 'roles.defs.targets_compliance',
-    tech 		= require 'roles.defs.tech',
-    testings 		= require 'roles.defs.testings',
-    tickets 		= require 'roles.defs._ro',
-    training_materials 	= require 'roles.defs._ro',
-    trainings 		= require 'roles.defs.trainings',
-    wishes 		= require 'roles.defs.wishes'
+M.permissions = { -- disable any objects exept *null*
+    null = { }
 }
 
 return M
