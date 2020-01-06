89ace2af4969f2ecef1a29214d457839b045edd9
Igor Artemov <i_artemov@omobus.net>
2020-01-06 19:21:41 +0300

    validate: added validatin [uids].

--- validate.lua
+++ validate.lua
@@ -3,29 +3,32 @@
 
 local M = {} -- public interface
 
+local core = require 'core'
+
 function M.isuid(str)
-    return str ~= nil and str:match('[^a-zA-ZабвгдеёжзийклмнопрстуфхцчшщъыьэюяА-Я0-9-.,_/: ()+]') == nil
+    return str ~= nil and str:match('[^a-zA-ZабвгдеёжзийклмнопрстуфхцчшщъыьэюяА-Я0-9-._/: ()+]') == nil
+end
+
+function M.isuids(str, delim)
+    local ar
+    if str == nil then
+	return false
+    end
+    ar = core.split(str, delim or ',')
+    if ar == nil or #ar == 0 then
+	return false
+    end
+    for i, val in ipairs(ar) do
+	if M.isuid(val) == false then
+	    return false
+	end
+    end
+
+    return true
 end
 
 function M.isdate(str)
     return str ~= nil and #str == 10 and str:match('((%d%d%d%d)-(%d%d)-(%d%d))') ~= nil
-end
-
-function M.equal(arr, arg)
-    if type(arg) == 'table' then
-	for key, value in pairs(arg) do
-	    if M.equal(arr, value) then return true end
-	end
-    else
-	if type(arr) == 'table' then
-	    for key, value in pairs(arr) do
-		if value == arg then return true end
-	    end
-	else
-	    return arr == arg and true or false
-	end
-    end
-    return false
 end
 
 return M
