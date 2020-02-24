23940071cbb88beb5044897acbe23085acc38385
Igor Artemov <i_artemov@omobus.net>
2020-02-24 10:27:33 +0300

    3.4.67 release

--- version.lua
+++ version.lua
@@ -4,7 +4,7 @@
 local M = {} -- public interface
 
 M.package_code		= "console"
-M.package_version	= "3.4.66"
+M.package_version	= "3.4.67"
 M.package_name		= "omobus-" .. M.package_code
 M.static_prefix		= "/" .. M.package_code .. "/" .. M.package_version
 
