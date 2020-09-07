60ece4e52415a7133c9b89cf73eb3d613b72a7cb
Igor Artemov <i_aetemov@omobus.net>
2020-09-07 12:31:38 +0300

    *: added [chan_id] to the [my_cities] and [my_regions] permissions checker.

--- plugins/posms.lua
+++ plugins/posms.lua
@@ -45,9 +45,9 @@ select account_id from my_accounts where user_id in (select my_staff(%user_id%, 
     union
 select account_id from my_retail_chains r, accounts a where r.user_id in (select my_staff(%user_id%, 1::bool_t)) and r.rc_id=a.rc_id and (r.region_id='' or r.region_id=a.region_id)
     union
-select account_id from my_regions r, accounts a where r.user_id in (select my_staff(%user_id%, 1::bool_t)) and r.region_id=a.region_id
+select account_id from my_regions r, accounts a where r.user_id in (select my_staff(%user_id%, 1::bool_t)) and r.region_id=a.region_id and (r.chan_id='' or r.chan_id=a.chan_id)
     union
-select account_id from (select expand_cities(city_id) city_id from my_cities where user_id in (select my_staff(%user_id%, 1::bool_t))) c, accounts a where c.city_id=a.city_id
+select account_id from (select expand_cities(city_id) city_id, chan_id from my_cities where user_id in (select my_staff(%user_id%, 1::bool_t))) c, accounts a where c.city_id=a.city_id and (c.chan_id='' or c.chan_id=a.chan_id)
 ]]
 		    , "//posms/F.accounts"
 		    , {user_id = sestb.erpid}
