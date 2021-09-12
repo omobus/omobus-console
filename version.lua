d857f27c5f84ded5eb4031fc9379ea4b56d20651
Igor Artemov ( <i_artemov@omobus.net>
2021-09-12 08:15:22 +0300

    3.5.23 release

--- version.lua
+++ version.lua
@@ -4,7 +4,7 @@
 local M = {} -- public interface
 
 M.package_code		= "console"
-M.package_version	= "3.5.22"
+M.package_version	= "3.5.23"
 M.package_name		= "omobus-" .. M.package_code
 M.static_prefix		= "/" .. M.package_code .. "/" .. M.package_version
 
